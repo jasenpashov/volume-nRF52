db82e4787e2ab86a20551f867d9bb9235af105f5
jasen <jasen@revauxy.com>
2026-03-16 13:55:41 +0200

    V2 power-saving

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+//tenstar robot nrf52840 pro micro
+
 // #include <Arduino.h>
 // #include <bluefruit.h>
 
@@ -83,9 +85,12 @@
 #include <bluefruit.h>
 
 #define ENCODER_CLK 11
-#define ENCODER_DT 12
-#define ENCODER_SW 1
-#define LED_PIN 15
+#define ENCODER_DT  12
+#define ENCODER_SW  1
+#define LED_PIN     15
+
+// Debounce за бутона
+#define SW_DEBOUNCE_MS 350
 
 BLEDis bledis;
 BLEHidAdafruit blehid;
@@ -94,43 +99,41 @@ BLEBas blebas;
 volatile int encoderDelta = 0;
 volatile int lastClkState;
 
-void readEncoder()
-{
+// За debounce на бутона
+volatile uint32_t lastSwPress = 0;
+
+void readEncoder() {
     int clkState = digitalRead(ENCODER_CLK);
-    if (clkState != lastClkState)
-    {
-        if (digitalRead(ENCODER_DT) != clkState)
-        {
+    if (clkState != lastClkState) {
+        if (digitalRead(ENCODER_DT) != clkState) {
             encoderDelta++;
-        }
-        else
-        {
+        } else {
             encoderDelta--;
         }
         lastClkState = clkState;
     }
 }
 
-void setup()
-{
-
-    // Serial.begin(115200); // Serial is stopped for economy.
+void setup() {
+    delay(2000);
 
     pinMode(LED_PIN, OUTPUT);
     digitalWrite(LED_PIN, LOW);
 
+    
     pinMode(ENCODER_CLK, INPUT);
-    pinMode(ENCODER_DT, INPUT);
-    pinMode(ENCODER_SW, INPUT_PULLUP);
+    pinMode(ENCODER_DT,  INPUT);
+    pinMode(ENCODER_SW,  INPUT_PULLUP);
 
     lastClkState = digitalRead(ENCODER_CLK);
-
-    // attach Interrupt
     attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), readEncoder, CHANGE);
 
     Bluefruit.begin();
-    Bluefruit.setTxPower(-12); // Save energy -12 is for about 2-3 meters 4 is the maximum
+    Bluefruit.setTxPower(-16);
     Bluefruit.setName("nRF52-Volume-LowPower");
+
+
+    Bluefruit.Periph.setConnInterval(80, 160); // 100ms–200ms
 
     bledis.setManufacturer("Logitech");
     bledis.setModel("nRF52-HID");
@@ -138,7 +141,6 @@ void setup()
 
     blebas.begin();
     blebas.write(100);
-
     blehid.begin();
 
     Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
@@ -146,45 +148,43 @@ void setup()
     Bluefruit.Advertising.addService(blehid);
     Bluefruit.Advertising.addService(blebas);
     Bluefruit.ScanResponse.addName();
+
     Bluefruit.Advertising.restartOnDisconnect(true);
-    Bluefruit.Advertising.setInterval(619, 619);
+    Bluefruit.Advertising.setInterval(2048, 2048);
     Bluefruit.Advertising.start(0);
 }
 
-void loop()
-{
-
-    if (!Bluefruit.connected())
-    {
-        waitForEvent();
+void loop() {
+    if (!Bluefruit.connected()) {
+        sd_app_evt_wait();
         return;
     }
 
-    if (digitalRead(ENCODER_SW) == LOW)
-    {
-        blehid.consumerKeyPress(HID_USAGE_CONSUMER_MUTE);
-        delay(50);
-        blehid.consumerKeyRelease();
-        delay(300);
+    
+    if (digitalRead(ENCODER_SW) == LOW) {
+        uint32_t now = millis();
+        if ((now - lastSwPress) > SW_DEBOUNCE_MS) {
+            lastSwPress = now;
+            blehid.consumerKeyPress(HID_USAGE_CONSUMER_MUTE);
+            delay(10);
+            blehid.consumerKeyRelease();
+        }
     }
 
-    if (encoderDelta >= 2)
-    {
-        blehid.consumerKeyPress(HID_USAGE_CONSUMER_VOLUME_INCREMENT);
-        delay(10);
-        blehid.consumerKeyRelease();
+    if (abs(encoderDelta) >= 1) {
+        noInterrupts();
+        int delta = encoderDelta;
         encoderDelta = 0;
-    }
-    else if (encoderDelta <= -2)
-    {
-        blehid.consumerKeyPress(HID_USAGE_CONSUMER_VOLUME_DECREMENT);
-        delay(10);
+        interrupts();
+
+        uint16_t key = (delta > 0)
+            ? HID_USAGE_CONSUMER_VOLUME_INCREMENT
+            : HID_USAGE_CONSUMER_VOLUME_DECREMENT;
+
+        blehid.consumerKeyPress(key);
+        delay(5);
         blehid.consumerKeyRelease();
-        encoderDelta = 0;
     }
 
-    // 3. PUT THE PROCESSOR TO SLEEP
-    // This function waits for the next interrupt or BLE event.
-    // It is an internal function specific to the nRF52 architecture.
-    waitForEvent(); 
+    sd_app_evt_wait();
 }
